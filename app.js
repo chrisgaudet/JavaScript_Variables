56b17c0e686df35ed7ef5d153a590b370a3c713d
Chris Gaudet <chrisgaudet9@gmail.com>
2025-10-21 14:26:15 -0500

    Completed JavaScript Variables Exercise.

--- app.js
+++ app.js
@@ -3,8 +3,39 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 & 2
+const firstName = "Chris";
+let lastName = "G";
+var age = 35;
 
-// Exercise 2
+lastName = "Charlie";
+age = 247;
+
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}.`);
+
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`The ${isOnSale} price is ${price}, marked down ${salePercentage} percent for ${selectedSize} sizes, but the stock is ${stock}, therfore to say that they have any is ${inStock}.`);
+
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`I love the book ${title} by ${author}. I have bookmarked page ${bookmark}, but the book's page count is ${pageCount}. I know this to be ${hasRead}.`);
